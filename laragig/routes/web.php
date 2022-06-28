4bc3257cd0fb153e1bf6fc3776dda8558b7871d6
majdhallak <majdhallak90@gmail.com>
2022-06-28 21:55:15 +0300

    @props added

--- laragig/routes/web.php
+++ laragig/routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-
 use App\Models\Listing;
 
 /* |-------------------------------------------------------------------------- | Web Routes |-------------------------------------------------------------------------- | | Here is where you can register web routes for your application. These | routes are loaded by the RouteServiceProvider within a group which | contains the "web" middleware group. Now create something great! | */
@@ -11,17 +9,14 @@ Route::get('/', function () {
     return view('listings',
     [
     'heading' => 'Latest Listing',
-    'listings' => Listing::all()
-
-    ]
-    );
+    'listings' => Listing::all(),
+    ]);
 });
 
 // Single Listing:
-Route::get('/listings/{id}', function ($id) {
+Route::get('/listings/{listingId}', function (Listing $listingId) {
     return view('listing', [
-    'id' => $id,
-    'listing' => Listing::find($id)
+    'listing' => Listing::find($listingId),
     ]);
 });
 
