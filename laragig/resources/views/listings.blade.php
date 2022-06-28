3f3010071d22b360a8d8171fcfb297557ff75fcc
majdhallak <majdhallak90@gmail.com>
2022-06-28 12:59:57 +0300

    _search.blade.php->partials_view

--- laragig/resources/views/listings.blade.php
+++ laragig/resources/views/listings.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+@include('partisals._hero')
+
 {{-- @php
 $test=1;
 // dd(count($listings))
@@ -22,7 +24,7 @@ test:{{$test}} --}}
                 />
                 <div>
                     <h3 class="text-2xl">
-                        <a href="show.html">{{$listing->title}}</a>
+                        <a href="/listings/'{{$listing->id}}'">{{$listing->title}}</a>
                     </h3>
                     <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
                     <ul class="flex">
